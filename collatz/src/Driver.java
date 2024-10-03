6f6d181e57f9945f0c6801b52e6003c025f56385
bixiphel <jacka10peil123@gmail.com>
2024-10-03 12:57:55 -0500

    Added a feature to display the number of steps a number takes.

--- collatz/src/Driver.java
+++ collatz/src/Driver.java
@@ -6,8 +6,11 @@ public class Driver {
 
         try {
             // Instantiates a Scanner (if you want certain elements) and a PrintWriter to write to an output
-            Scanner infile = new Scanner(new File("src\\\\input.txt"));
+            Scanner infile = new Scanner(new File("src\\\\testSet.txt"));
             PrintWriter outfile = new PrintWriter("src\\\\output.txt");
+
+            // Creates a stats object to get any stats for testing or data.
+            Stats stats = new Stats();
 
             // Iterates over a text input file
             while(infile.hasNextLine()) {
@@ -15,7 +18,8 @@ public class Driver {
                 int number = Integer.parseInt(infile.nextLine());
 
                 // Creates a temporary string to hold the sequence of steps it takes to reduce to 1
-                String pattern = "" + number;
+                String temp = "Number: " + number;
+                String pattern = number + "";
 
                 // Iteratively applies the collatz rule (half even numbers, multiply odd numbers by 3 and add 1)
                 while(number != 1 && number != 0) {
@@ -29,14 +33,18 @@ public class Driver {
                         number = (3 * number) + 1;
                     }
                     // Adds the current number into the temp. string holding the sequence 
-                    pattern = pattern + ", " + number;
+                    pattern = pattern + " -> " + number;
+                    stats.increment();
                 }
 
-                // Writes the sequence out to the output file
-                outfile.println(pattern);
+                // Writes the sequence out to the output file with the number of steps it takes to reach 1.
+                outfile.println(temp + " | Count: " + stats.getCount() + " | Sequence: " + pattern);
+
+                // Resets the count back to zero before moving on to the next integer in the input file.
+                stats.setCount(0);
             }
 
-            // Closes the scanner and output file
+            // Closes the scanner and output file.
             outfile.close();
             infile.close();
 
