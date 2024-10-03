0511ea50cd3bc6d72e1044a26b1c533730e1502c
bixiphel <jacka10peil123@gmail.com>
2024-10-03 10:34:05 -0500

    Fixed the issue with outputting to a file; results will now write to an output file.

--- collatz/src/Driver.java
+++ collatz/src/Driver.java
@@ -9,23 +9,32 @@ public class Driver {
             Scanner infile = new Scanner(new File("src\\\\input.txt"));
             PrintWriter outfile = new PrintWriter("src\\\\output.txt");
 
+            // Iterates over a text input file
             while(infile.hasNextLine()) {
+                // Parses the next integer from a text input file (formatted as 1 integer per line)
                 int number = Integer.parseInt(infile.nextLine());
-                String pattern = "";
 
-                while(number != 1) {
+                // Creates a temporary string to hold the sequence of steps it takes to reduce to 1
+                String pattern = "" + number;
+
+                // Iteratively applies the collatz rule (half even numbers, multiply odd numbers by 3 and add 1)
+                while(number != 1 && number != 0) {
                     if(number % 2 == 0) {
-                        number *= 2;
+                        number = number / 2;
+                    } else if(number == 1){
+                        // Breaks the loop if the current number is 1 since this is the end of the sequence.
+                        pattern = pattern + "1.";
+                        break;
                     } else {
                         number = (3 * number) + 1;
                     }
-                    pattern = number + ", ";
+                    // Adds the current number into the temp. string holding the sequence 
+                    pattern = pattern + ", " + number;
                 }
 
+                // Writes the sequence out to the output file
                 outfile.println(pattern);
             }
-
-            outfile.println("hello :)");
 
             // Closes the scanner and output file
             outfile.close();
